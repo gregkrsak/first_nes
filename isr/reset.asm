0eac6fae7d88075bdae335dac6005f3de31a0f0e
Greg Krsak <greg.krsak@gmail.com>
2018-01-29 21:46:53 -0800

    Updated header/footer comments to reflect migration to ca65

--- isr/reset.asm
+++ isr/reset.asm
@@ -1,10 +1,10 @@
 ;
 ; first_nes
-; reset.asm
+; reset.s
 ;
 ; This is the RESET Interrupt Service Routine for the first_nes project.
 ;
-; Written by Greg M. Krsak <greg.krsak@gmail.com>, Feb. 2017
+; Written by Greg M. Krsak <greg.krsak@gmail.com>, 2018
 ;
 ; Based on the NintendoAge "Nerdy Nights" tutorials, by bunnyboy:
 ;   http://nintendoage.com/forum/messageview.cfm?catid=22&threadid=7155
@@ -14,19 +14,20 @@
 ;   https://emu-docs.org/NES/nestech.txt
 ;
 ; Processor: 8-bit, Ricoh RP2A03 (6502), 1.789773 MHz (NTSC)
-; Assembler: NESASM 3.1
+; Assembler: ca65 (cc65 binutils)
 ;
 ; Tested with:
-; nesasm3 "first_nes.asm"
+; (untested)
 ;
 ; Tested on:
-; Nestopia 1.40
+; (untested)
 ;
 ; For more information about NES programming in general, try these references:
 ; https://en.wikibooks.org/wiki/NES_Programming
 ;
-; For more information on the NESASM assembler, try these references:
-; http://www.nespowerpak.com/nesasm/usage.txt
+; For more information on the ca65 assembler, try these references:
+; https://github.com/cc65/cc65
+; http://cc65.github.io/doc/ca65.html
 ;
 
 
@@ -151,4 +152,4 @@ ENDISRReset:
 __ISRVerticalBlankBegin:
 
 
-; End of reset.inc
+; End of reset.s
